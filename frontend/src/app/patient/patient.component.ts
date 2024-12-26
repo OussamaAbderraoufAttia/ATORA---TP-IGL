818a1610dd0f62d67ca16dd6c4f5fd04dbb704c2
luka modric <mr_sahraoui@esi.dz>
2024-12-26 13:40:14 +0100

    update component

--- frontend/src/app/patient/patient.component.ts
+++ frontend/src/app/patient/patient.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-patient',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './patient.component.html',
   styleUrl: './patient.component.css'
 })
 export class PatientComponent {
   activeTab: string = 'personal';
   userRole:string = "admin" // Default state
+  isModalOpen = false;
+  modalTitle = '';
+  modalHeaders: string[] = [];
+  modalRows: string[][] = [];
 
   setActiveTab(tab: string): void {
     this.activeTab = tab;
@@ -20,6 +25,33 @@ export class PatientComponent {
 
   getRole():string{
     return this.userRole;
+  }
+  handleCardClick(category: string) {
+    this.modalTitle = category;
+    this.modalHeaders = this.getTableHeaders(category);
+    this.modalRows = []; // No data rows for now
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  getTableHeaders(category: string): string[] {
+    switch (category) {
+      case "Consultations":
+        return ['ID', 'Date', 'Résumé', 'Actions'];
+      case "Bilans Biologique":
+        return ['ID', 'Date', 'Laboratoire', 'Actions'];
+      case "Soins":
+        return ['ID', 'Date', 'Description', 'Actions'];
+      case "Diagnostiques":
+        return ['ID', 'Date', 'Diagnostic', 'Actions'];
+      case "Bilans Radio":
+        return ['ID', 'Date', 'Examen', 'Actions'];
+      default:
+        return [];
+    }
   }
 
   
